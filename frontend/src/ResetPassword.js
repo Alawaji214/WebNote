09cb3fbbdc9743fbcc427b8fb800c7282d231020
Mohammad Alawaji <moh214011@gmail.com>
2023-12-26 23:30:44 +0300

    Update ResetPassword.js

--- frontend/src/ResetPassword.js
+++ frontend/src/ResetPassword.js
@@ -9,7 +9,7 @@ const ResetPassword = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const response = await fetch('/v1/reset/reset-password/${token}', {
+    const response = await fetch('https://note-gateway3-2m0hc1vk.uc.gateway.dev/v1/reset/reset-password/${token}', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
